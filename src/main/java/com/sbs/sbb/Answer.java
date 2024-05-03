c7326727009987997fca719f23ed6f7cc80973b8
tron0318 <tron0318@naver.com>
2024-05-03 15:44:31 +0900

    Answer Entity

---
+++ src/main/java/com/sbs/sbb/Answer.java
@@ -0,0 +1,26 @@
+package com.sbs.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+
+    // private Inteager qeustionId;
+    @ManyToOne
+    private Question question;
+}
