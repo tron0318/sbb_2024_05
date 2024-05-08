7bd1788ae16ab2fcef3eb0c7468544c4e74679a9
tron0318 <tron0318@naver.com>
2024-05-08 15:43:29 +0900

    리다이렉트

--- src/main/java/com/sbs/sbb/MainController.java
+++ src/main/java/com/sbs/sbb/MainController.java
@@ -11,4 +11,9 @@ public class MainController {
     public String index() {
         return "안녕하세요!!!";
     }
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
+    }
 }
