69f2704bec01a592f31cbb52191c00283833e847
tron0318 <tron0318@naver.com>
2024-05-08 17:47:08 +0900

    답변등록 ê폼 구현중

--- src/main/java/com/sbs/sbb/question/QuestionController.java
+++ src/main/java/com/sbs/sbb/question/QuestionController.java
@@ -3,17 +3,17 @@ package com.sbs.sbb.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequestMapping("/question")
 @Controller
 @RequiredArgsConstructor
 public class QuestionController {
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model) {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
@@ -21,12 +21,18 @@ public class QuestionController {
         return "question_list";
     }
 
-    @GetMapping("/question/detail/{id}")
+    @GetMapping("/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
         Question q = this.questionService.getQuestion(id);
 
         model.addAttribute("question", q);
 
         return "question_detail";
+    }
+
+    @PostMapping("/createTemp")
+    @ResponseBody
+    public String temp(){
+        return "전송완료";
     }
 }
