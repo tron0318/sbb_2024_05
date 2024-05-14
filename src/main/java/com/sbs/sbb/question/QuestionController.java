868219daf1e9c1b547d17c83e319dd5dbce8f642
tron0318 <tron0318@naver.com>
2024-05-14 17:34:45 +0900

    질문삭제

--- src/main/java/com/sbs/sbb/question/QuestionController.java
+++ src/main/java/com/sbs/sbb/question/QuestionController.java
@@ -105,4 +105,15 @@ public class QuestionController {
         questionForm.setContent(question.getContent());
         return "question_form";
     }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String questionDelete(Principal principal, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        if (!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+        }
+        this.questionService.delete(question);
+        return "redirect:/";
+    }
 }
