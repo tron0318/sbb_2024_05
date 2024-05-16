c4c289bf85567116ebc81313f84385c5257de1c5
tron0318 <tron0318@naver.com>
2024-05-16 14:40:10 +0900

    추천기능

--- src/main/java/com/sbs/sbb/question/QuestionService.java
+++ src/main/java/com/sbs/sbb/question/QuestionService.java
@@ -63,4 +63,9 @@ public class QuestionService {
     public void delete(Question question) {
         this.questionRepository.delete(question);
     }
+
+    public void vote(Question question, SiteUser voter) {
+        question.addVoter(voter);
+        this.questionRepository.save(question);
+    }
 }
